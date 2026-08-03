df3cc27aabfd7a1450fc5536c54c9c367f8b53b2
Daniel Reck <danielreckdesouza1@gmail.com>
2026-08-03 09:16:27 -0300

    Adapta interface inicial para Cantina Federal

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
+
 const indexRoutes = require('./routes/indexRoutes');
 const userRoutes = require('./routes/userRoutes');
 const produtoRoutes = require('./routes/produtoRoutes');
-const categoriaRoutes = require('./routes/categoriaRoutes');
 const pedidoRoutes = require('./routes/pedidoRoutes');
 
 const app = express();
-app.locals.title = 'CRUD';
 const PORT = process.env.PORT || 3000;
+
+app.locals.title = 'Cantina Federal';
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
 app.use(expressLayouts);
 app.use(express.static(__dirname + '/public'));
 
@@ -21,12 +25,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// Rotas
 app.use('/', indexRoutes);
+
+// Por enquanto continua /users.
+// Depois vamos renomear para /clientes.
 app.use('/users', userRoutes);
+
 app.use('/produtos', produtoRoutes);
-app.use('/categorias', categoriaRoutes);
 app.use('/pedidos', pedidoRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Cantina Federal rodando em http://localhost:${PORT}`);
 });
